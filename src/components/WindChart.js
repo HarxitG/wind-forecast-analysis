85a3eb60529921953104929f1ab95b919a62b7fc
Harshit Soni <h_soni@mt.iitr.ac.in>
2026-03-14 19:04:54 +0530

    Update chart axis labels, spacing and time formatting to match challenge UI

--- src/components/WindChart.js
+++ src/components/WindChart.js
@@ -1,44 +1,91 @@
-"use client";
-
 import {
   LineChart,
   Line,
   XAxis,
   YAxis,
-  Tooltip,
   CartesianGrid,
+  Tooltip,
   Legend,
   ResponsiveContainer,
 } from "recharts";
 
 export default function WindChart({ data }) {
+  const formatTime = (value) => {
+    const date = new Date(value);
+    return date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  };
+
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
+    <div style={{ marginTop: "40px" }}>
+      <ResponsiveContainer width="100%" height={450}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 60,
+            bottom: 60,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
 
-        <XAxis dataKey="time" />
+          {/* X AXIS */}
+          <XAxis
+            dataKey="time"
+            tickFormatter={formatTime}
+            minTickGap={40}
+            label={{
+              value: "Target Time End (UTC)",
+              position: "bottom",
+              offset: 20,
+            }}
+          />
 
-        <YAxis />
+          {/* Y AXIS */}
+          <YAxis
+            label={{
+              value: "Power (MW)",
+              angle: -90,
+              position: "insideLeft",
+              style: { textAnchor: "middle" },
+            }}
+          />
 
-        <Tooltip />
+          <Tooltip />
 
-        <Legend />
+          {/* LEGEND */}
+          <Legend
+            verticalAlign="bottom"
+            align="center"
+            wrapperStyle={{
+              bottom: 0,
+              paddingTop: "25px",
+            }}
+          />
 
-        <Line
-          type="monotone"
-          dataKey="actual"
-          stroke="#0000ff"
-          strokeWidth={2}
-        />
+          <Line
+            type="monotone"
+            dataKey="actual"
+            stroke="#2f7ed8"
+            strokeWidth={2}
+            dot={false}
+            name="actual"
+          />
 
-        <Line
-          type="monotone"
-          dataKey="forecast"
-          stroke="#00aa00"
-          strokeWidth={2}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+          <Line
+            type="monotone"
+            dataKey="forecast"
+            stroke="#2ca02c"
+            strokeWidth={2}
+            dot={false}
+            name="forecast"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
